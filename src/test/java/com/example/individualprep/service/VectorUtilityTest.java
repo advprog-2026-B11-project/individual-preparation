26582d9f90d503956b95bab597fdaa10611214fe
Karla Ameera Raswanda <karla.raswanda@gmail.com>
2026-02-14 10:59:08 +0700

    Update VectorUtilityTest.java

--- src/test/java/com/example/individualprep/service/VectorUtilityTest.java
+++ src/test/java/com/example/individualprep/service/VectorUtilityTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class VectorUtilityTest {
+
     @Test
     void multiply_shouldMultiplyEachElementByScalar() {
         VectorUtility util = new VectorUtility();
@@ -19,13 +20,14 @@ class VectorUtilityTest {
     void multiply_nullVector_shouldThrow() {
         VectorUtility util = new VectorUtility();
         assertThrows(IllegalArgumentException.class, () -> util.multiply(null, 3));
-      
+    }
+
     @Test
     void testDotProductDifferentLength_ThrowsException() {
         VectorUtility vectorUtility = new VectorUtility();
         double[] v1 = {1.0, 2.0};
         double[] v2 = {1.0, 2.0, 3.0};
-        
+
         Exception exception = assertThrows(IllegalArgumentException.class, () -> {
             vectorUtility.dotProduct(v1, v2);
         });
